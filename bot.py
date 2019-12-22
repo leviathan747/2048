50b5ce0688232811c0f17caa455646182b16c71b
Levi Starrett <levi@roxsoftware.com>
2019-12-21 23:43:43 -0500

    update bot

--- bot.py
+++ bot.py
@@ -3,13 +3,25 @@ import flask
 
 app = flask.Flask(__name__)
 
-lastMove = 0
+lastMove = 2
+lastBoard = None
+numMatches = 0
 
 @app.route('/move', methods=['POST'])
 def getMove():
-    global lastMove
+    global lastMove, lastBoard, numMatches
     data = json.loads(flask.request.get_data())
-    lastMove = 0 if lastMove != 0 else 3
+    if data['cells'] == lastBoard:
+        numMatches += 1
+    else:
+        numMatches = 0
+    lastBoard = data['cells']
+    if numMatches >= 2:
+        # go left to break out of stalemate
+        lastMove = 0
+    else:
+        # alternate between right and down
+        lastMove = 3 if lastMove != 3 else 2
     resp = flask.Response(json.dumps({'move': lastMove}))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
